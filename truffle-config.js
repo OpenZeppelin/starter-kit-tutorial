642b463043bba330facf7bf6dff8bee4fea8d2a4
Daniel Constantin <hello@danielconstantin.net>
2019-08-14 15:18:24 +0300

    Update truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -31,14 +31,14 @@ module.exports = {
       gasPrice: 10000000000,
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 4,
       gas: 3000000,
       gasPrice: 10000000000
     },
     // main ethereum network(mainnet)
     main: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 1,
       gas: 3000000,
       gasPrice: 10000000000
